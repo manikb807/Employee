09ba4822d5c7362d60785c5ed91274f79db37326
hasdit <baraimanik08@gmail.com>
2025-11-21 11:37:41 +0600

    add Task10

--- Constants.java
+++ Constants.java
@@ -1,16 +1,17 @@
 // File Name: Constants.java
 
-public class Constants {
+// This class contains all constants used in the EmployeeManager program
 
-    public static final String EMPLOYEE_FILE = "employees.txt";
-    public static final String ERROR_ARGUMENTS = "Error: Exactly one argument required.";
-    public static final String USAGE_MESSAGE
-            = "Usage:\n"
-            + "  l          - Load employees\n"
-            + "  s          - Show random employee\n"
-            + "  +Name      - Add employee\n"
-            + "  ?Name      - Search employee\n"
-            + "  uName      - Update employee\n"
-            + "  dName      - Delete employee\n"
-            + "  c          - Count words";
+public class Constants {
+    public static final String EMPLOYEE_FILE = "employees.txt";   // File path to store employee names
+    public static final String ERROR_ARGUMENTS = "Error: Exactly one argument required."; // Error message for invalid argument count
+    public static final String USAGE_MESSAGE = 
+            "Usage:\n" +
+            "  l          - Load employees\n" +
+            "  s          - Show random employee\n" +
+            "  +Name      - Add employee\n" +
+            "  ?Name      - Search employee\n" +
+            "  uName      - Update employee\n" +
+            "  dName      - Delete employee\n" +
+            "  c          - Count words";
 }
